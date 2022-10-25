80cea5152ee53d491d565f813bb3963a8bdd56e3
Giniatsu <110588543+Giniatsu@users.noreply.github.com>
2022-10-26 04:57:40 +0800

    ListView created, connected to firebase firestore

--- server.js
+++ server.js
@@ -6,9 +6,29 @@ const port = process.env.PORT || 3000;
 app.listen(port,);
 app.set('view engine', 'ejs');
 
+var fs = require("firebase-admin");
+let serviceAccount;
+if(process.env.GOOGLE_CREDENTIALS != null){
+    serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS)
+}else{
+    serviceAccount = require("./jpgundamtoycollectibles-firebase-adminsdk-jj4f7-40ee22633d.json")
+}
+fs.initializeApp({
+    credential: fs.credential.cert(serviceAccount)
+});
+
+const db = fs.firestore();
+const ingColl = db.collection('items');
+
 app.get('/', async function (req, res){
+    const items = await ingColl.get();
+    // console.log(items.docs.length);
+    // items.forEach(doc => {
+    //     console.log(doc.data());
+    // })
     let data = {
         url: req.url,
+        itemData: items.docs,
     }
     res.render('pages/index', data);
 });
